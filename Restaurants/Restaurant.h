15ebfb3de7028bcde2539cd76bb05aa20e2b0cbe
Nicole Del Senno <ncdelsenno@gmail.com>
2012-02-27 20:42:09 -0500

    added array

--- Restaurants/Restaurant.h
+++ Restaurants/Restaurant.h
@@ -12,12 +12,15 @@
     NSString* cuisineType;
     int yearOpened;
     
+    NSMutableArray* reviews;
+    
     
 }
 @property(readwrite, strong) NSString* address;
 @property(readwrite, strong) NSString* name;
 @property(readwrite, strong) NSString* cuisineType;
 @property(readwrite) int yearOpened;
+@property(readwrite, strong) NSMutableArray* reviews;
 
 -(int)age;
 
