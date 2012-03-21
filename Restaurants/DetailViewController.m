14a6cd957f09d8c84154887d816c473b8b71c264
Nicole Del Senno <ncdelsenno@gmail.com>
2012-03-21 00:55:17 -0400

    Handled and tested edge cases successfully

--- Restaurants/DetailViewController.m
+++ Restaurants/DetailViewController.m
@@ -37,35 +37,35 @@
     review1.text = @"What fab-u-lass chicken! We could eat it all day if we didn't have to stop to drink sangria!";
     review1.reviewerName = @"The Addams";
     review1.score = 5;
-    review1.numberOfHelpfulRatings = 19;
-    review1.numberOfUnhelpfulRatings = 8;
+    review1.numberOfHelpfulRatings = 12;
+    review1.numberOfUnhelpfulRatings = 7;
     
     Review* review2 = [[Review alloc] init];
     review2.text = @"I DONE POSTED ON DA INTARWEBS!";
     review2.reviewerName = @"Anonymous";
     review2.score = 1;
-    review2.numberOfHelpfulRatings = 0;
-    review2.numberOfUnhelpfulRatings = 45;
+    review2.numberOfHelpfulRatings = 2;
+    review2.numberOfUnhelpfulRatings = 6;
     
     Review* review3 = [[Review alloc] init];
     review3.text = @"Some of the best chicken I've ever eaten. A helpful tip: get some green (Aji) sauce to go, they sell it by the pint!";
     review3.reviewerName = @"Jim Carr";
     review3.score = 5;
-    review3.numberOfHelpfulRatings = 28;
+    review3.numberOfHelpfulRatings =18;
     review3.numberOfUnhelpfulRatings = 2;
     
     Review* review4 = [[Review alloc] init];
     review4.text = @"While the food is amazing, they often simply don't pick up the phone when ordering out!";
     review4.reviewerName = @"Paul";
     review4.score = 4;
-    review4.numberOfHelpfulRatings = 14;
+    review4.numberOfHelpfulRatings = 12;
     review4.numberOfUnhelpfulRatings = 5;
     
     Review* review5= [[Review alloc] init];
     review5.text= @"I cannot stand this place. Food is disgusting and service is horrendous!";
     review5.reviewerName = @"Nicole";
     review5.score = 1;
-    review5.numberOfHelpfulRatings = 30;
+    review5.numberOfHelpfulRatings = 5;
     review5.numberOfUnhelpfulRatings = 1;
 
  
@@ -82,7 +82,16 @@
     Review* review = [restaurant mostHelpfulReview];
     
     helpfulReviewLabel.text = [review text];
+    
+    if ([review text] == nil) {
+        helpfulReviewLabel.text = [NSString stringWithFormat:@"Not enough helpful reviews yet."];
+    }
+    
     helpfulReviewPercentageLabel.text = [NSString stringWithFormat:@"Most helpful review - %i of %i found this review helpful", review.numberOfHelpfulRatings, [review total]];
+    
+    if([review total] == 0){
+        helpfulReviewPercentageLabel.text = [NSString stringWithFormat:@""];
+    }
     
  
     
